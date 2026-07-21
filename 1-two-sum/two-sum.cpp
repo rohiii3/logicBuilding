1f677e50e1fb2b50d647e641fb9a187999df42aa
Rohini Wagh <147098891+rohiii3@users.noreply.github.com>
2026-07-21 10:08:41 +0530

    Time: 201 ms (5.00%) | Memory: 14.2 MB (64.58%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> sol;
+        
         for(int j=0;j<nums.size();j++){
+            int comp=target-nums[j];
             for(int i=0;i<nums.size();i++){
-                if(nums[j]+nums[i]==target && i!=j && sol.size()<2){
+                if(nums[i]==comp && i!=j && sol.size()<2){
                     sol.push_back(j);
                     sol.push_back(i);
                 }
