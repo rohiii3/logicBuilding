f348c1cebfebe28bac1f28b2c637639f4f4386c6
Rohini Wagh <147098891+rohiii3@users.noreply.github.com>
2026-07-31 15:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.32%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -3,19 +3,16 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> ans;
         for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0){
+            if(i%3==0 && i%5==0)
                 ans.push_back("FizzBuzz");
-            }
-            else if(i%5==0){
+            else if(i%5==0)
                 ans.push_back("Buzz");
-            }
             else if(i%3==0){
                 ans.push_back("Fizz");
             }
             else
                 ans.push_back(to_string(i));
         }
-        return ans;
-        
+        return ans;  
     }
 };
