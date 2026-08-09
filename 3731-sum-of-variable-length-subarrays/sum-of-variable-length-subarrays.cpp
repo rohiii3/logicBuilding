61532fd2e412d04c7e22708c7d9ba6bc9409d88b
Rohini Wagh <147098891+rohiii3@users.noreply.github.com>
2026-08-10 00:20:14 +0530

    Time: 6 ms (3.65%) | Memory: 40.1 MB (5.72%) - LeetSync

--- 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.cpp
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums) {
         int s = 0;
-
         for (int i = 0; i < nums.size(); i++) {
             vector<int> v;
             int j = max(0, i - nums[i]);
